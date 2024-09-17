909d70b33114087926128f6ad9fefcdb9acbdf75
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-09-17 00:50:35 +0000

    hi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
 st.title("😎 My Ultra Super-Dooper Cool Mega app😎 ")
-st.write(
-    "Hello! This app is so cool right?? That is because it was made in Uncle Zergei's Homeschool Programing Club for Girls! But my cousin, wants to be a quitter. "
-)
-
+st.write("""
+Hello! This app is so cool right?? That is because it was made in 
+Uncle Zergei's Homeschool Programing Club for Girls! But my cousin, 
+wants to be a quitter.
+         
+   ### Click play on one of these if you think she shouldn't and hear an Ultra Super-Dooper Cool Mega song!
+""")
+st.audio("https://www.televisiontunes.com/uploads/audio/Frozen%20-%20Let%20it%20Go.mp3")
+st.audio("https://www.televisiontunes.com/uploads/audio/Veggie%20Tales%20-%20The%20Hairbrush%20Song.mp3") 
