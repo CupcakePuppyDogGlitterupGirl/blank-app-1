394cd784a545aa5f1d452b5ebaec343d12cb7830
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-09-28 20:48:53 +0000

    yay

--- pages/student_registration.py
+++ pages/student_registration.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import datetime
 
 st.title("😎 Welcome to Uncle Zergei's Homeschool Programing Club for Girls!")
 
@@ -19,9 +20,24 @@ full_name = st.text_input("What is your full name?")
 
 age = st.slider("What is your age?")
 
-birthday = st.date_input("When is your birthday?")
+start_date = datetime.date(2000, 1, 1)
+end_date = datetime.date(2020, 12, 31)
+birthday = st.date_input("When is your birthday?", min_value=start_date, max_value=end_date)
+
 
 picture = st.camera_input("Take a picture")
+
+sports = st.multiselect(
+    "What sports have you played?",
+    ["⚽️ soccer", "🏀 basketball", "🏈 football", "⚾️ baseball", "🎾 tennis", "🏐 volley-ball", "🏉 rugby",
+    "🏓 ping-pong", "🏒 hockey", "🏏 cricket" ,"🛹 skate-boarding" ,"🛼 rollar-skating" ,"🛷 sleding", "⛸️ ice-skating", "⛷️ skiing", "🥊 boxing" ,"🥋 karate", "🪂 parachuting", "🤸‍♀️ gymnastics" ,"🏄🏻‍♀️ surfing", "🏇 horseback-riding",
+    "🏊🏻 swimming" ,"🩰 ballet" ,"🚣🏻‍♀️ rowing", "🧗‍♀️ rock-climbing" ,"🚴🏻‍♂️ biking" ,"🤹‍♀️ juggling"],
+)
+color = st.color_picker("Fav Color")
+
+visit = st.checkbox("Have you been on the official school visit?")
+
+moodeng = st.checkbox("Do you think Moo Deng is ADORABLE:")
 
 
 
@@ -33,4 +49,12 @@ st.write("Age:", age)
 st.write("Birthday:", birthday)
 if picture:
     st.image(picture, width=100)
-
+st.write("Sports:\n\n", "\n\n".join(sports))
+st.write("Favorite Color:", color)
+st.color_picker("Your Favorite Color:", value = color, disabled = True)
+st.write("Have been on the school visit:",visit)
+st.write("Loves Moo Deng:", moodeng)
+if moodeng == True:
+    st.write(":rainbow[Hip Hippo Horay!]🦛😍🎉")
+else:
+    st.write(":red[How DARE you!!]🤯😱")
