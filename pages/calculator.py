9ff7cd897eedbc59c809b5e26190ee6e623be239
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-10-04 16:44:05 +0000

    birthdayyay

---
+++ pages/calculator.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Felicity's Magnificent Calculator")
+
+num1 = st.number_input("num1", value=4)
+
+num2 = st.number_input("num2", value=5)
+
+
+st.button("Reset", type="primary")
+if st.button("add"):
+    st.write("num1 + num2 =", num1+num2)
+if st.button
