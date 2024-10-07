a4792a4b8a7a3a02632a6e3f8d350df04b6bafe5
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-10-07 12:26:46 +0000

    blablabla

--- pages/calculator.py
+++ pages/calculator.py
@@ -10,4 +10,11 @@ num2 = st.number_input("num2", value=5)
 st.button("Reset", type="primary")
 if st.button("add"):
     st.write("num1 + num2 =", num1+num2)
-if st.button
+if st.button("Multiply"):
+    st.write("num1 * num2 =", num1*num2)
+
+if st.button("minus"):
+    st.write("num1 - num2 =", num1-num2)
+if st.button("Divideeeeeeee"):
+    st.write("num1 / num2 =", num1/num2)
+    st.balloons()
