6772406136be0afa4857a3d1c23d2c7e8b92124d
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-10-11 16:55:37 +0000

    i love baby mice

--- pages/Dice.py
+++ pages/Dice.py
@@ -1,28 +1,28 @@
 import streamlit as st
 import random
 
-def get_grade(test_score):
-    if test_score == 100:
-        grade = "A+"
-    elif test_score >= 90:
-        grade = "A"
-    elif test_score >= 80:
-        grade = "B"
-    elif test_score >= 70:
-        grade = "C"
+def get_roll(roll):
+    if roll == 8:
+        roll = "6"
+    elif roll >= 5:
+        roll = "4"
+    elif roll >= 3:
+        roll = "3"
+    elif roll >= 1:
+        roll = "1"
     else:
-        grade = "F"
-    return grade
+        roll = "2"
+    return roll
 
-test_score = st.slider("What is your test score?")
+roll = st.slider("How many sides does this dice have?")
 
-grade = get_grade(test_score)
+roll = get_roll(roll)
 
-st.write(grade)
+st.write(roll)
 
 # HOMEWORK: Make the get_roll function work!
 
-st.write("Roll two dice and get the result")
+st.write("Roll one dice and get the result")
 
 num_sides = st.slider("How many sides should your dice have?", min_value=2, max_value=20)
 
