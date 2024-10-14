81d9a11c32640ab08c6ce9974b48d6e62ee1e6df
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-10-14 21:25:50 +0000

    bob is the best name ever

--- pages/Dice.py
+++ pages/Dice.py
@@ -1,17 +1,20 @@
 import streamlit as st
 import random
 
-def get_roll(num_sides):
-    roll = random.randint(1, num_sides)
-    roll2 = random.randint(1, num_sides)
-    return roll2 + roll
+def get_roll(num_sides, num_dice):
+    total = 0 
+    for i in range(num_dice):
+        roll = random.randint(1, num_sides)
+        total = total+roll
+    
+    return total
 # HOMEWORK: Make the get_roll function work!
 
 st.write("Roll two dice and get the result")
 
 num_sides = st.slider("How many sides should your dice have?", min_value=2, max_value=20)
-
-roll = get_roll(num_sides)
+num_dice = st.slider("How many dice do you want to roll?")
+roll = get_roll(num_sides, num_dice)
 
 st.write("Roll is:", roll)
 
