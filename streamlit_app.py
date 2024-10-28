dce9cd407ec30d06e5bfb905836241b5042a0710
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-10-28 12:52:16 +0000

    hellomynameislarrythecucumberbuticantsinganymore

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,4 @@ wants to be a quitter.
    ### Click play on one of these if you think she shouldn't and hear an Ultra Super-Dooper Cool Mega song!
 """)
 st.audio("Frozen_Let_it_Go.mp3")
-st.audio("https://www.televisiontunes.com/uploads/audio/Veggie%20Tales%20-%20The%20Hairbrush%20Song.mp3") 
+st.audio("icecream.mp3") 
