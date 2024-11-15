9f35cc9c6a3a281da5d34b92e476f9bbf1929e8f
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-11-15 19:33:12 +0000

    this is not workind but i can't fix it!Q

--- pages/weather_forcast.py
+++ pages/weather_forcast.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.balloons
+st.balloons()
+st.snow()
+def emojis():
+    rain(
+        emoji="🎈",
+        font_size=54,
+        falling_speed=5,
+        animation_length="infinite",
+    )
+pip install emojis
