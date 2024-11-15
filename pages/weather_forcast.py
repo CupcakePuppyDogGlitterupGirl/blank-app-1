f1bca48e633ce9235dce0d4dc0404d0d2b288b23
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-11-15 20:22:44 +0000

    Merry Christmas!

--- pages/weather_forcast.py
+++ pages/weather_forcast.py
@@ -1,13 +1,17 @@
+from streamlit_extras.let_it_rain import rain 
+
 import streamlit as st
 
 st.balloons()
 st.snow()
 def emojis():
     rain(
-        emoji="🥶, 🥳",
+        emoji="❄️ ",
         font_size=54,
         falling_speed=5,
-        animation_length="infinite",
+        animation_length="infinite"
     )
     
-pip install emojis
+emojis()
+
+st.title("It looks like everyone's white Christmas dreams will come true this year! Happy Holidays! This is San Taclause, from Ultra Sooper-Dooper Cool Mega News; signing off!")
