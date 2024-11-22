05ad54be1dd7154b1f230c68882f467e174f2c17
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-11-22 20:31:05 +0000

    This is not working so I am giving up but not really because i will try again on Monday before class. And doing this right now is just making me sad. So I  need to eat some ice cream and watch a fun movie and send gifs.byeee

--- pages/Schedule.py
+++ pages/Schedule.py
@@ -1,12 +1,23 @@
 import streamlit as st
-st.write("Hello☺️! This is a helpful daily schedule to help you be more: Fabulously Encouraging, Lovely, Incredible, Careing, Incomprehendibly Trustworthy, Yummilicious!")
+st.write("Hello☺️! This is a helpful daily schedule to help you be more: Fabulously Encouraging, Lovely, Incredible, Careing, Incomprehendibly Trustworthy, Yummilicious!") 
 
-st.checkbox("Wake up")
+output = st.checkbox("Brush your teeth")
+st.write(output)
 
-st.checkbox("brush teeth")
-st.checkbox("brush hair")
-checkbox = st.checkbox("Enable custom input")
-if checkbox:
-    user_input = st.text_input("Enter your custom text")
-    if user_input:
-        st.write(f: st.checkbox("user_input")")
+title = st.text_input("Add your own", "(example, have a Christmas party)")
+
+
+if "my_stuff" not in st.session_state:
+    st.session_state["my_stuff"] = [title]
+
+for checkbox in st.session_state["my_stuff"]
+
+if st.button("add a thing"):
+    st.session_state["my_stuff"].append(title)
+whatever = st.checkbox(title)
+st.write(whatever)
+
+st.write("SESSION STATE:")
+st.write(str(st.session_state))
+
+
