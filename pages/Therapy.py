34563e7a293020c69e6268a123a1e73fd9165f21
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-12-02 23:10:39 +0000

    hefwoihgfwklklhjifhlrsfuhwiuefgiwuefhwoefwekfjbnkwejbfkjewbhfjerbfuwbhrfukwhberfjw

--- pages/Therapy.py
+++ pages/Therapy.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 st.title("Welcome to Therapy with Felica and Kate!")
 st.write("We hope that you can complete the following and become filled with joy and become rejuvenated!")
 image_urls = [
@@ -14,14 +15,17 @@ with st.chat_message("user"):
     st.link_button("show dog Photo 3:", image_urls[-1])
 with st.expander("Click here to see options for animals up for adoption."):
      st.image("https://static.streamlit.io/examples/dog.jpg")
-     random.choice("https://static.streamlit.io/examples/dog.jpg")
-(222 kB)
-https://hips.hearstapps.com/hmg-prod/images/little-cute-maltipoo-puppy-royalty-free-image-1652926025.jpg
+image_pick=random.choice(image_urls)
+st.image(image_pick)
+     
 
-(661 kB)
-https://upload.wikimedia.org/wikipedia/commons/4/47/American_Eskimo_Dog.jpg
+#(222 kB)
+#https://hips.hearstapps.com/hmg-prod/images/little-cute-maltipoo-puppy-royalty-free-image-1652926025.jpg
 
-(213 kB)
-https://upload.wikimedia.org/wikipedia/commons/b/b2/Dog-1123016_960_720.jpg
+#(661 kB)
+#hfttps://upload.wikimedia.org/wikipedia/commons/4/47/American_Eskimo_Dog.jpg
 
-(95 kB)
+#(213 kB)
+#https://upload.wikimedia.org/wikipedia/commons/b/b2/Dog-1123016_960_720.jpg
+
+#(95 kB)
