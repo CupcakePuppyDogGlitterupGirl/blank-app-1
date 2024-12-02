47893cb3a1a50bb5642a68ee64273faebf283bc3
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-12-02 22:36:08 +0000

    fhowfhuwefh

--- pages/Schedule.py
+++ pages/Schedule.py
@@ -1,23 +1,23 @@
 import streamlit as st
 st.write("Hello☺️! This is a helpful daily schedule to help you be more: Fabulously Encouraging, Lovely, Incredible, Careing, Incomprehendibly Trustworthy, Yummilicious!") 
 
-output = st.checkbox("Brush your teeth")
-st.write(output)
+#output = st.checkbox("Brush your teeth")
+#st.write(output)
+if "my_stuff" not in st.session_state:
+    st.session_state["my_stuff"] = ["Clean Room", "Check Mailbox"]
 
 title = st.text_input("Add your own", "(example, have a Christmas party)")
-
-
-if "my_stuff" not in st.session_state:
-    st.session_state["my_stuff"] = [title]
-
-for checkbox in st.session_state["my_stuff"]
-
 if st.button("add a thing"):
     st.session_state["my_stuff"].append(title)
-whatever = st.checkbox(title)
-st.write(whatever)
 
-st.write("SESSION STATE:")
-st.write(str(st.session_state))
+
+for checkbox in st.session_state["my_stuff"]:
+    st.checkbox(checkbox)
+
+
+
+
+#st.write("SESSION STATE:")
+#st.write(str(st.session_state))
 
 
