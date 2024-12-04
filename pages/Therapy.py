571e29ba4c979bffd93065fc71060e12d2162b63
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-12-04 17:07:16 +0000

    I love puppies! I LOVE puppies! Oh yeah!

--- pages/Therapy.py
+++ pages/Therapy.py
@@ -3,20 +3,24 @@ import random
 st.title("Welcome to Therapy with Felica and Kate!")
 st.write("We hope that you can complete the following and become filled with joy and become rejuvenated!")
 image_urls = [
-     ('https://hips.hearstapps.com/hmg-prod/images/little-cute-maltipoo-puppy-royalty-free-image-1652926025.jpg'),
-     ('https://upload.wikimedia.org/wikipedia/commons/4/47/American_Eskimo_Dog.jpg'),
-     ('https://upload.wikimedia.org/wikipedia/commons/b/b2/Dog-1123016_960_720.jpg')
+     'https://hips.hearstapps.com/hmg-prod/images/little-cute-maltipoo-puppy-royalty-free-image-1652926025.jpg',
+     'https://upload.wikimedia.org/wikipedia/commons/4/47/American_Eskimo_Dog.jpg',
+     'https://upload.wikimedia.org/wikipedia/commons/b/b2/Dog-1123016_960_720.jpg',
+     'cutiedoggiegiphy1.gif',
+     'mowhawk doggie.jpg',
+     'gifdog1.gif',
+     'doggif1.gif',
+     'cutedoggif1.gif'
 ]
 with st.chat_message("user"):
-    st.write("If you want to experience a moment of joy click the button and see some amazing dog pictures!!")
-    st.button("Reset", type="primary")
-    st.link_button("show dog Photo 1:", image_urls[0])
-    st.link_button("show dog Photo 2:", image_urls[1])
-    st.link_button("show dog Photo 3:", image_urls[-1])
+    st.write("If you want to experience a moment of joy and unforgetable adorability, click the button!!")
+    
+    st.button("The Button")
+image_pick=random.choice(image_urls)
+st.image(image_pick, width=450)   
 with st.expander("Click here to see options for animals up for adoption."):
      st.image("https://static.streamlit.io/examples/dog.jpg")
-image_pick=random.choice(image_urls)
-st.image(image_pick)
+
      
 
 #(222 kB)
