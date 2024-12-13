bc57fcd8b3a73088968efb529de1e15ae6f9e8ba
CupcakePuppyDogGlitterupGirl <felicity@blackwoods.us>
2024-12-13 20:29:02 +0000

    Camera

---
+++ pages/Camera.py
@@ -0,0 +1,12 @@
+
+
+import streamlit as st
+
+picture = st.camera_input("Take a picture")
+
+if picture:
+    st.image(picture)
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
